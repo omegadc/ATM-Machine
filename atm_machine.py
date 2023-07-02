22f1cbabff2c2c9289b975aade29e5a0bf0a7338
omegadc <dcleader99@gmail.com>
2023-07-02 01:10:59 -0400

    Implemented sqlite3 to create a small database

--- atm_machine.py
+++ atm_machine.py
@@ -1,3 +1,20 @@
 # hello this is the main atm file. There will be others included. 
+import sqlite3
 
-print("hello")
+con = sqlite3.connect("test.db")
+cur = con.cursor()
+# cur.execute("CREATE TABLE Account(account_num, year, balance)")
+
+#verifying that the new table has been created by querying the SQLITE_MASTER table built-in to SQLite
+
+res = cur.execute("SELECT name FROM sqlite_master")
+res.fetchone()
+
+cur.execute("""
+        INSERT INTO Account VALUES
+        (101, 2023, 550.0)
+        """)
+con.commit()
+
+res = cur.execute("SELECT account_num FROM Account")
+res.fetchall()
